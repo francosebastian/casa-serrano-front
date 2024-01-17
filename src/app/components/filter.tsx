76a9c8e999c93f24fbc0aecc9f5fd13ccb59a283
Franco Arrano Silva <farrano@kdu.cl>
2024-01-17 13:48:26 -0300

    cambio de titulo del filtro

--- src/app/components/filter.tsx
+++ src/app/components/filter.tsx
@@ -4,10 +4,10 @@ export default function Filter({ onChange, categories }: any) {
     return (
         <div className="p-6 grid gap-2 md:grid-cols-2">
 
-            <label htmlFor="countries" className="block mb-2 text-sm font-ultra text-gray-900 dark:text-white">Filtrar por tipo</label>
+            <label htmlFor="countries" className="block mb-2 text-sm font-ultra text-gray-900 dark:text-white">Filtrar por Categoria</label>
             <div>
                 <select onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" aria-label="Filter Countries By Countries">
-                    <option value="All">Filtrar por Tipo</option>
+                    <option value="All">Categorias</option>
                     {
                         categories.map((registro: any) =>
                             <>
